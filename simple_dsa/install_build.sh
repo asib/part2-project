7b06ea857d23c3ed0941f49c904fba678aaf378c
Jacob Fenton <jacob.d.fenton@gmail.com>
2017-11-16 16:26:16 +0000

    I've written a very simple applet that responds to a challenge with its AES 128 encryption. I'm now starting work on a very simple DSA applet.

---
+++ simple_dsa/install_build.sh
@@ -0,0 +1,1 @@
+ant jc && gpshell gpinstall.txt
---
+++ simple_dsa/src/main/java/org/bitbucket/jfent/simple_dsa/SimpleDigitalSignatureApplet.java
@@ -0,0 +1,89 @@
+package org.bitbucket.jfent.simple_dsa;
+
+import javacard.framework.Applet;
+import javacard.framework.APDU;
+import javacard.framework.Util;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
+import javacard.framework.SystemException;
+import javacard.security.KeyBuilder;
+import javacard.security.AESKey;
+import javacard.security.CryptoException;
+import javacardx.crypto.Cipher;
+
+public class SimpleDigitalSignatureApplet extends Applet {
+  /*
+   *
+   * Constants
+   *
+   */
+  final static byte CHALLENGE = (byte)0x01; // INS byte for Challenge
+  final static byte[] KEY_DATA = {(byte)0xeb, (byte)0x0e, (byte)0x36, (byte)0x2c,
+                                  (byte)0x21, (byte)0xeb, (byte)0x3e, (byte)0x47,
+                                  (byte)0xf9, (byte)0xa2, (byte)0xeb, (byte)0x2f,
+                                  (byte)0xba, (byte)0x9e, (byte)0x30, (byte)0xd7};
+  final static short EXPECTED_CHALLENGE_LENGTH = (short)0x10;
+  final static short EXPECTED_CIPHER_LENGTH = (short)0x10;
+
+  /*
+   *
+   * Instance variables
+   *
+   */
+  private AESKey key;
+  private Cipher cipher;
+  // Need somewhere to put the output of encryption/decryption.
+  private byte[] cipherOutput;
+
+  private SimpleDigitalSignatureApplet() {
+    key = (AESKey)KeyBuilder.buildKey(KeyBuilder.TYPE_AES,
+        KeyBuilder.LENGTH_AES_128, false);
+    key.setKey(KEY_DATA, (short)0);
+    cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+
+    cipherOutput = JCSystem.makeTransientByteArray(EXPECTED_CHALLENGE_LENGTH,
+        JCSystem.CLEAR_ON_DESELECT);
+
+    register();
+  }
+
+  public static void install(byte[] bArray, short bOffset, byte bLength) {
+      new SimpleDigitalSignatureApplet();
+  }
+
+  public void process(APDU apdu) {
+    byte[] buffer = apdu.getBuffer();
+
+    if (!apdu.isISOInterindustryCLA()) {
+      switch (buffer[ISO7816.OFFSET_INS]) {
+        case CHALLENGE:
+          processChallenge(apdu);
+          break;
+      }
+    }
+  }
+
+  private void processChallenge(APDU apdu) {
+    byte[] buffer = apdu.getBuffer();
+
+    short unreadChallengeBytes = buffer[ISO7816.OFFSET_LC];
+    if (unreadChallengeBytes != EXPECTED_CHALLENGE_LENGTH)
+      ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+
+    apdu.setIncomingAndReceive();
+
+    // Setup the cipher to be in encryption mode.
+    try {
+      cipher.init(key, Cipher.MODE_ENCRYPT);
+      cipher.doFinal(buffer, ISO7816.OFFSET_CDATA, EXPECTED_CHALLENGE_LENGTH,
+          cipherOutput, (short)0);
+    } catch (CryptoException e) {
+      ISOException.throwIt(e.getReason());
+    }
+    Util.arrayCopyNonAtomic(cipherOutput, (short)0, buffer, (short)0,
+        EXPECTED_CIPHER_LENGTH);
+
+    apdu.setOutgoingAndSend((short)0, EXPECTED_CIPHER_LENGTH);
+  }
+}
